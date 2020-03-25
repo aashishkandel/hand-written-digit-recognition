08edc20d967e87cbc46ea69e4a35d633790d880c
aashish kandel <kandelaashish0001@gmail.com>
2020-03-25 19:11:00 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,281 @@
+//global variable declaration
+let model;
+
+var canvasWidth           	= 300;
+var canvasHeight 			= 300;
+var canvasStrokeStyle		= "white";
+var canvasLineJoin			= "round";
+var canvasLineWidth       	= 7;
+var canvasBackgroundColor 	= "black";
+var canvasId              	= "canvas";
+
+var clickX = new Array();
+var clickY = new Array();
+var clickD = new Array();
+var drawing;
+
+
+canvas = document.getElementById('draw_box');
+
+canvas.setAttribute("width", canvasWidth);
+canvas.setAttribute("height", canvasHeight);
+canvas.style.backgroundColor = canvasBackgroundColor;
+ctx = canvas.getContext("2d");
+
+
+//---------------------
+// MOUSE DOWN function
+//---------------------
+canvas.addEventListener("mousedown",function(e) {
+	var rect = canvas.getBoundingClientRect();
+	var mouseX = e.clientX- rect.left;;
+	var mouseY = e.clientY- rect.top;
+	drawing = true;
+	addUserGesture(mouseX, mouseY);
+	drawOnCanvas();
+});
+
+//-----------------------
+// TOUCH START function
+//-----------------------
+canvas.addEventListener("touchstart", function (e) {
+	if (e.target == canvas) {
+    	e.preventDefault();
+  	}
+
+	var rect = canvas.getBoundingClientRect();
+	var touch = e.touches[0];
+
+	var mouseX = touch.clientX - rect.left;
+	var mouseY = touch.clientY - rect.top;
+
+	drawing = true;
+	addUserGesture(mouseX, mouseY);
+	drawOnCanvas();
+
+}, false);
+
+//---------------------
+// MOUSE MOVE function
+//---------------------
+canvas.addEventListener("mousemove",(function(e) {
+	if(drawing) {
+		var rect = canvas.getBoundingClientRect();
+		var mouseX = e.clientX- rect.left;;
+		var mouseY = e.clientY- rect.top;
+		addUserGesture(mouseX, mouseY, true);
+		drawOnCanvas();
+	}
+}));
+
+//---------------------
+// TOUCH MOVE function
+//---------------------
+canvas.addEventListener("touchmove", function (e) {
+	if (e.target == canvas) {
+    	e.preventDefault();
+  	}
+	if(drawing) {
+		var rect = canvas.getBoundingClientRect();
+		var touch = e.touches[0];
+
+		var mouseX = touch.clientX - rect.left;
+		var mouseY = touch.clientY - rect.top;
+
+		addUserGesture(mouseX, mouseY, true);
+		drawOnCanvas();
+	}
+}, false);
+
+//-------------------
+// MOUSE UP function
+//-------------------
+canvas.addEventListener("mouseup",function(e) {
+	drawing = false;
+});
+
+//---------------------
+// TOUCH END function
+//---------------------
+canvas.addEventListener("touchend", function (e) {
+	if (e.target == canvas) {
+    	e.preventDefault();
+  	}
+	drawing = false;
+}, false);
+
+//----------------------
+// MOUSE LEAVE function
+//----------------------
+canvas.addEventListener("mouseleave",function(e) {
+	drawing = false;
+});
+
+//-----------------------
+// TOUCH LEAVE function
+//-----------------------
+canvas.addEventListener("touchleave", function (e) {
+	if (e.target == canvas) {
+    	e.preventDefault();
+  	}
+	drawing = false;
+}, false);
+
+//--------------------
+// ADD CLICK function
+//--------------------
+function addUserGesture(x, y, dragging) {
+	clickX.push(x);
+	clickY.push(y);
+	clickD.push(dragging);
+}
+
+//-------------------
+// RE DRAW function
+//-------------------
+function drawOnCanvas() {
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+	ctx.strokeStyle = canvasStrokeStyle;
+	ctx.lineJoin    = canvasLineJoin;
+	ctx.lineWidth   = canvasLineWidth;
+
+	for (var i = 0; i < clickX.length; i++) {
+		ctx.beginPath();
+		if(clickD[i] && i) {
+			ctx.moveTo(clickX[i-1], clickY[i-1]);
+		} else {
+			ctx.moveTo(clickX[i]-1, clickY[i]);
+		}
+		ctx.lineTo(clickX[i], clickY[i]);
+		ctx.closePath();
+		ctx.stroke();
+	}
+}
+
+//------------------------
+// CLEAR CANVAS function
+//------------------------
+var click = document.querySelector("#clear-button")
+
+click.addEventListener("click",(async function () {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+	clickX = new Array();
+	clickY = new Array();
+	clickD = new Array();
+	document.querySelector(".prediction-text").innerText = "you cleared";
+}));
+
+//-------------------------------------
+// loader for cnn model
+//-------------------------------------
+async function loadModel() {
+  console.log("model loading..");
+
+  // clear the model variable
+  model = undefined;
+  
+  // load the model using a HTTPS request (where you have stored your model files)
+  model = await tf.loadLayersModel("model/model.json");
+  
+  console.log("model loaded..");
+
+}
+
+loadModel();
+
+//-----------------------------------------------
+// preprocess the canvas
+//-----------------------------------------------
+function preprocessCanvas(image) {
+	// resize the input image to target size of (1, 28, 28)
+	let tensor = tf.browser.fromPixels(image)
+		.resizeNearestNeighbor([28, 28])
+		.mean(2)
+		.expandDims(2)
+		.expandDims()
+		.toFloat();
+	console.log(tensor.shape);
+	return tensor.div(255.0);
+}
+
+//--------------------------------------------
+// predict function 
+//--------------------------------------------
+document.querySelector("#predict-button").addEventListener("click",(async function () {
+    // get image data from canvas
+	var imageData = canvas.toDataURL();
+
+	// preprocess canvas
+	let tensor = preprocessCanvas(canvas);
+
+	// make predictions on the preprocessed image tensor
+	let predictions = await model.predict(tensor).data();
+
+	// get the model's prediction results
+	let results = Array.from(predictions);
+
+	// display the predictions in chart
+	displayChart(results);
+	displayLabel(results);
+
+	console.log(results);
+}));
+
+//------------------------------
+// Chart to display predictions
+//------------------------------
+var chart = "";
+var firstTime = 0;
+function loadChart(label, data, modelSelected) {
+	var ctx = document.getElementById('chart_box').getContext('2d');
+	chart = new Chart(ctx, {
+	    // The type of chart we want to create
+	    type: 'bar',
+
+	    // The data for our dataset
+	    data: {
+	        labels: label,
+	        datasets: [{
+	            label: "prediction via "+ modelSelected + " model",
+	            backgroundColor: '#f50057',
+	            borderColor: 'rgb(255, 99, 132)',
+	            data: data,
+	        }]
+	    },
+
+	    // Configuration options go here
+	    options: {}
+	});
+}
+
+//----------------------------
+// display chart with updated
+// drawing from canvas
+//----------------------------
+function displayChart(data) {
+  	var select_option = "CNN";
+
+	label = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+	if (firstTime == 0) {
+		loadChart(label, data, select_option);
+		firstTime = 1;
+	} else {
+		chart.destroy();
+		loadChart(label, data, select_option);
+	}
+	document.getElementById('chart_box').style.display = "block";
+}
+
+function displayLabel(data) {
+	var max = data[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < data.length; i++) {
+        if (data[i] > max) {
+            maxIndex = i;
+            max = data[i];
+        }
+    }
+	document.querySelector(".prediction-text").innerHTML = "I think you drew <b>"+maxIndex+"</b>, and I am <b>"+Math.trunc( max*100 )+"%</b> confident"
+}
